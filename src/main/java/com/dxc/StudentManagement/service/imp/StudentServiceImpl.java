ea8d35060d2f1024498b4aac99464f02b01e1ab2
Brian Pek <pekchin.siongbrian@dxc.com>
2022-09-08 12:06:49 +0800

    Initial commit

---
+++ src/main/java/com/dxc/StudentManagement/service/imp/StudentServiceImpl.java
@@ -0,0 +1,46 @@
+package com.dxc.StudentManagement.service.imp;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dxc.StudentManagement.entity.Student;
+import com.dxc.StudentManagement.repository.StudentRepository;
+import com.dxc.StudentManagement.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	private StudentRepository studentRepository;
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);	
+	}
+
+}
